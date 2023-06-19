d928a5700d7c72df6ed3688d9ccb5787610fbfc2
steven91-ar <stevensinpra@gmail.com>
2023-06-19 14:12:18 +0200

    sur la route de spring boot

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,28 @@
 package com.wildcodeschool.doctor.controller;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
 
 @Controller
 public class DoctorController {
-
-    @GetMapping("/doctor/")
-    @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
+
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    public Doctor getDoctor(@PathVariable ("number") int number) {
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number >= 1 && number <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
+    }
+}
 }
